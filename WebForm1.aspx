57f66c86f3ae1c6288b37674fd03a0677467f6d3
DuncanKane <47971814+DuncanKane@users.noreply.github.com>
2019-07-16 18:06:19 +0100

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="loginform.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Username:
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                <table class="auto-style1">  
+            <tr>  
+                <td colspan="6" style="text-align: center; vertical-align: top">
+                    &nbsp;</td>  
+            </tr>  
+            </table> 
+        
+            Password:
+                    <asp:TextBox ID="TextBox2" runat="server" TextMode="Password"></asp:TextBox>
+            <p>
+            <asp:Button ID="Button2" runat="server" Text="Login" OnClick="Button2_Click" />
+                <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+        </div>
+    </form>
+    
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient; //this namespace is for sqlclient server  
+using System.Configuration; // this namespace is add I am adding connection name in web config file config connection name
+
+namespace loginform
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ToString());
+
+            try
+            {
+                string uid = TextBox1.Text;
+                string pass = TextBox2.Text;
+                con.Open();
+                string qry = "SELECT * FROM LoginDetails WHERE Username='" + uid + "' AND Password='" + pass + "'";
+                SqlCommand cmd = new SqlCommand(qry, con);
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if (sdr.Read())
+                {
+                    Label1.Text = "Login Sucess......!!";
+                }
+                else
+                {
+                    Label1.Text = "UserId & Password Is not correct Try again..!!";
+
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+        }
+
+    }
+    
+}
